0b4bd05a7ea015d62e6dc52f2c0212ee359e7303
leoferamos <fernandesramosleonardo@gmail.com>
2025-02-28 14:08:04 -0300

    Refatorando menu

--- src/main.py
+++ src/main.py
@@ -1,38 +1,37 @@
 import auth
 import os
 
-def limparTela():
-    sistemaOperacional = os.name 
-    if sistemaOperacional != 'nt': 
-        os.system('clear')
-    else: 
-        os.system('cls')
-
+def limpar_tela():
+    os.system('cls' if os.name == 'nt' else 'clear')
 
 def menu():
     while True:
+        limpar_tela()
         print("\n1. Registrar novo usuário")
         print("2. Fazer login")
         print("3. Listar usuários")
+        
         print("4. Sair")
 
         opcao = input("Escolha uma opção: ")
 
         if opcao == "1":
-            limparTela()
+            limpar_tela()
             username = input("Digite um nome de usuário: ")
             password = input("Digite uma senha: ")
             print(auth.register_user(username, password))
+            input("\nPressione Enter para continuar...")
 
         elif opcao == "2":
-            limparTela()
+            limpar_tela()
             username = input("Digite seu nome de usuário: ")
             password = input("Digite sua senha: ")
             token = input("Digite o código TOTP do seu aplicativo autenticador: ")
             print(auth.verificar_login(username, password, token))
+            input("\nPressione Enter para continuar...")
 
         elif opcao == "3":
-            limparTela()
+            limpar_tela()
             usuarios = auth.listar_usuarios()
             if usuarios:
                 print("\nUsuários cadastrados:")
@@ -40,6 +39,7 @@ def menu():
                     print(f"ID: {user[0]}, Username: {user[1]}")
             else:
                 print("Nenhum usuário cadastrado.")
+            input("\nPressione Enter para continuar...")
 
         elif opcao == "4":
             print("Saindo...")
@@ -47,6 +47,7 @@ def menu():
 
         else:
             print("Opção inválida. Tente novamente.")
+            input("\nPressione Enter para continuar...")
 
 if __name__ == "__main__":
     menu()
